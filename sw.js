5c394055928597fa7bb470d74ddb1576b84cc474
Gáyan Justo de Moraes <gayan.justo@gmail.com>
2020-07-22 20:05:58 -0300

    . alteração imagens SEO

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-10c2b8f15ed26f19b633.js"
+    "url": "webpack-runtime-75aa613705fa5ecc0561.js"
   },
   {
     "url": "framework-63ec46a3540d83159f28.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d4a4808d78cb32247813169b196b2fdb"
+    "revision": "ff94b02461c62563a7d50306edbf0822"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
